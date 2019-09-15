d53191ced0bac1c69c4ff4b98b905542cf9f4bcf
arvindbehera <arvind.behera@yahoo.co.in>
2019-09-16 00:29:41 +0530

    Add files via upload

---
+++ precache-manifest.d022aa6d39bac4cf97cfd87962c9b1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de7e1843f5bcbb194b358d8690b6d20",
+    "url": "/roboapp/index.html"
+  },
+  {
+    "revision": "10f626daac2ff6ffebb1",
+    "url": "/roboapp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7c549574f4a3fdb37f7a",
+    "url": "/roboapp/static/css/main.063c14d6.chunk.css"
+  },
+  {
+    "revision": "10f626daac2ff6ffebb1",
+    "url": "/roboapp/static/js/2.550ad6b9.chunk.js"
+  },
+  {
+    "revision": "7c549574f4a3fdb37f7a",
+    "url": "/roboapp/static/js/main.7e520c7c.chunk.js"
+  },
+  {
+    "revision": "38c4d64608979cfe5799",
+    "url": "/roboapp/static/js/runtime~main.9ecc00fd.js"
+  }
+]);
